0e36f2b55b95dfae19132ab97c82e19dd2cd1537
AshokKumar1718 <ashokkumar1708200@gmail.com>
2021-11-29 14:07:05 +0000

    contact list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularapp';
+  Name:string = '';
+  email: string ='';
+  phone:number=0;
+  image:string ='';
+  contacts =[];
+addContacts(){
+  console.log(this.Name,this.email,this.phone,this.image)
+  
+  if(this.Name && this.email && this.phone) {
+    this.contacts.push({
+      name:this.Name,
+      email:this.email,
+      phone:this.phone,
+      image:this.image?this.image:null,
+    })
+  }else{
+    alert("Enter All Fields")
+  }
+
+  console.log(this.contacts)
+}
+
+
 }
